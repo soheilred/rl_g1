502492092d82a4ab31507a3cacaf812542d4eaa1
soheilred <soheilred@yahoo.com>
2018-09-12 10:41:01 -0400

    generated files on 2018-09-12 10:41:01

--- ValueIteration.py
+++ ValueIteration.py
@@ -1,7 +1,7 @@
 # @Author: soheil
 # @Date:   2018-09-11 11:47:34
 # @Last Modified by:   soheilred
-# @Last Modified time: 2018-09-12 10:00:22
+# @Last Modified time: 2018-09-12 10:23:40
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -13,11 +13,11 @@ import sys, os
 def valueIteration(S, A, P, R, gamma):
 	v2 = np.zeros([len(S)])
 	v1 = np.zeros([len(S)])
-	V = v1
 	i = 0
-	eps = 0.0001
+	eps = 0.01
 	while (i is 0) or (np.linalg.norm(v2 - v1) > eps):
-		v1 = v2
+		i = i + 1
+		v1 = np.copy(v2)
 		for s in S:
 			Q = np.zeros(len(A))
 			for a in A:
@@ -26,14 +26,13 @@ def valueIteration(S, A, P, R, gamma):
 					Q[a] = Q[a] + P[s, sp, a] * ( R[s, sp, a] + gamma*v1[sp] )
 				print 'Q[a =',a ,',s =', s,'] =',Q[a]
 			v2[s] = np.max(Q)
-
-		np.concatenate((V, v2), axis=0)
+		
 		# twodecimals = ["%.2f" % var for var in V[len(V) - 1]]
 		# print (twodecimals)
-		print (v2)
+		print i , ',', v2
 		print (np.linalg.norm(v2 - v1))
-		i = 1
-	print (V)
+
+
 def main():
 	# try:
 	#     fileName = sys.argv[1]
